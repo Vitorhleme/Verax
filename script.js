a271553deba16041a3cfa14de67c5f7787f800f7
Vitorhleme <vitorhugolsenai6@gmail.com>
2025-10-16 15:43:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Inicializa a biblioteca Animate On Scroll (AOS)
+    AOS.init({
+        duration: 800,       // Duração da animação em ms
+        easing: 'ease-in-out', // Efeito de aceleração
+        once: true,          // Animação acontece apenas uma vez
+        offset: 100,         // Distância do elemento para iniciar a animação
+    });
+
+});
